2052346d32884472e2ec5479b0627c2d20422d36
Taeyang Jin <sun@jupiterflow.com>
2021-03-28 09:11:04 +0900

    feat: Index Controller Test

---
+++ src/main/java/kr/dataportal/datahubservice/controller/IndexController.java
@@ -0,0 +1,24 @@
+/*
+    Copyright (c) 2021 Aaron(JIN, Taeyang).
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Lesser General Public License, version 3
+    which accompanies this distribution, and is available at
+    https://www.gnu.org/licenses/lgpl-3.0.html
+    
+    Contributors:
+        Aaron(JIN, Taeyang) - 
+*/
+
+package kr.dataportal.datahubservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String Index() {
+        return "index";
+    }
+}
