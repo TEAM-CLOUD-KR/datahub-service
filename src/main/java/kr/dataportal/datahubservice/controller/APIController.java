3dc82ace77697042329c05de9b033ee21f4aeec2
Taeyang Jin <sun@jupiterflow.com>
2021-03-30 13:45:54 +0900

    Update APIController.java

--- src/main/java/kr/dataportal/datahubservice/controller/APIController.java
+++ src/main/java/kr/dataportal/datahubservice/controller/APIController.java
@@ -13,11 +13,13 @@ package kr.dataportal.datahubservice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class APIController {
 
-    @GetMapping("/api_list")
+    @GetMapping("/list")
     public String ApiList() {
         return "api_list";
     }
