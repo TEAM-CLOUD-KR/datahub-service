7daa0dce8484ddc7f71f4423bec8e37918c3d189
Taeyang Jin <sun@jupiterflow.com>
2021-04-07 09:08:22 +0900

    feat: convertObjectToList Method

--- src/main/java/kr/dataportal/datahubservice/controller/TestController.java
+++ src/main/java/kr/dataportal/datahubservice/controller/TestController.java
@@ -11,6 +11,8 @@
 
 package kr.dataportal.datahubservice.controller;
 
+import kr.dataportal.datahubservice.domain.datacore.JSONResponse;
+import kr.dataportal.datahubservice.util.CommonUtil;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,19 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
     @GetMapping("/{target}")
-    public Object Test(@PathVariable String target) {
+    public List<?> Test(@PathVariable String target) {
         WebClient client = WebClient.builder()
                 .baseUrl("https://api.dataportal.kr")
                 .build();
 
-        return client.get()
+        JSONResponse result = client.get()
                 .uri("/common/util/scheme/{target}", target)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
-                .bodyToMono(Object.class);
+                .bodyToFlux(JSONResponse.class)
+                .toStream()
+                .collect(Collectors.toList()).get(0);
+
+        return CommonUtil.convertObjectToList(result.getData());
     }
 }
---
+++ src/main/java/kr/dataportal/datahubservice/util/CommonUtil.java
@@ -0,0 +1,18 @@
+package kr.dataportal.datahubservice.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class CommonUtil {
+    public static List<?> convertObjectToList(Object obj) {
+        List<?> list = new ArrayList<>();
+        if (obj.getClass().isArray()) {
+            list = Arrays.asList((Object[])obj);
+        } else if (obj instanceof Collection) {
+            list = new ArrayList<>((Collection<?>)obj);
+        }
+        return list;
+    }
+}
