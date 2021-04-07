f6df642e72a59104d037115856fe4055a0d6b4d9
Taeyang Jin <sun@jupiterflow.com>
2021-04-07 16:59:20 +0900

    refactor: API List Page

--- src/test/java/kr/dataportal/datahubservice/DatahubServiceApplicationTests.java
+++ src/test/java/kr/dataportal/datahubservice/DatahubServiceApplicationTests.java
@@ -1,7 +1,5 @@
 package kr.dataportal.datahubservice;
 
-import kr.dataportal.datahubservice.domain.user.User;
-import kr.dataportal.datahubservice.service.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,27 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
         "optional:/Users/sun/repository/_secrets/datahub-service.yml"
 ))
 class DatahubServiceApplicationTests {
-    @Autowired
-    private UserService userService;
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    @Transactional(readOnly = false)
-    @Rollback(value = true)
-    void 회원가입테스트() {
-        User user_1 = User.create("test@example.com", "pass1", "pass1", "abc");
-        Assertions.assertThat(user_1).isNotNull();
-        Assertions.assertThat(user_1.getEmail()).isEqualTo("test@example.com");
-
-        int signup_1 = userService.signup(user_1);
-        Assertions.assertThat(signup_1).isNotNull();
-
-        Assertions.assertThat(userService.findByEmail(user_1.getEmail()).getNickname()).isEqualTo(user_1.getNickname());
-
-        User user_2 = User.create("test@example.com", "pass1", "pass2", "abc");
-        Assertions.assertThat(user_2).isNull();
     }
 }
