a24c0008f440c5a85ac09b56f40c192b7c8ec48e
Taeyang Jin <sun@jupiterflow.com>
2021-04-27 12:25:46 +0900

    Update ApiListSearchDTO.java

--- src/main/java/kr/dataportal/datahubservice/dto/api/ApiListSearchDTO.java
+++ src/main/java/kr/dataportal/datahubservice/dto/api/ApiListSearchDTO.java
@@ -1,30 +1,33 @@
 package kr.dataportal.datahubservice.dto.api;
 
-import kr.dataportal.datahubservice.dto.PermissionGroup;
-import kr.dataportal.datahubservice.dto.common.Category1st;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Getter
 public class ApiListSearchDTO {
     private final Integer page;
     private final Integer itemPerPage;
-    private final PermissionGroup permissionGroup;
-    private final String category;
-    private final String organization;
+    private final List<String> ownDatahub;
+    private final List<String> category;
+    private final List<String> organization;
+    private final String name;
 
-    public ApiListSearchDTO(Integer page, Integer itemPerPage, PermissionGroup permissionGroup, String category, String organization) {
+    public ApiListSearchDTO(Integer page, Integer itemPerPage, List<String> ownDatahub,
+                            List<String> category, List<String> organization, String name) {
         this.page = Objects.requireNonNullElse(page, 1);
         this.itemPerPage = Objects.requireNonNullElse(itemPerPage, 10);
-        this.permissionGroup = Objects.requireNonNullElse(permissionGroup, PermissionGroup.PERMISSION_PUBLIC);
-        this.category = Objects.requireNonNullElse(category, "");
-        this.organization = Objects.requireNonNullElse(organization, "");
+        this.ownDatahub = Objects.requireNonNullElse(ownDatahub, new ArrayList<String>());
+        this.category = Objects.requireNonNullElse(category, new ArrayList<String>());
+        this.organization = Objects.requireNonNullElse(organization, new ArrayList<String>());
+        this.name = Objects.requireNonNullElse(name, "");
     }
 
     public static ApiListSearchDTO createDefault() {
-        return new ApiListSearchDTO(null, null, null, null, null);
+        return new ApiListSearchDTO(null, null, null, null,
+                null, null);
     }
 }
